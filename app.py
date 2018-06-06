444b0a2573cef65104854f31e77af732011437d9
Aragorn Dai <tzjie.dai@gmail.com>
2018-06-07 00:49:20 +0800

    Trying get requests

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 def handle_message(event):
         line_bot_api.reply_message(
 	        event.reply_token,
-                TextSendMessage(text = str(request) + app_response.output(event.message.text))
+                TextSendMessage(text = str(request.get_data(as_text = True)) + app_response.output(event.message.text))
         )
 
 if __name__ == '__main__':
