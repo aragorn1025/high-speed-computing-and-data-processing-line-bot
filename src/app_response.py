4ae48dbeff586bc53a5e36daff6c2068029d65f7
Aragorn Dai <tzjie.dai@gmail.com>
2018-06-07 02:05:43 +0800

    Get json text

--- src/app_response.py
+++ src/app_response.py
@@ -5,16 +5,18 @@ import instruction
 
 def output(user_id, inputs):
         try:
-                j = append_json(user_id, inputs)
-                return str(j) + '\n' + instruction.get_instruction_response(inputs)
+                if user_id = 'Ufb68b93dd139ad3ed7a318305a15b281' and inputs = 'json':
+                        return append_json(user_id, inputs, to_return = True)
+                return instruction.get_instruction_response(inputs)
         except Exception as e:
                 return 'Error:\n' + str(e)
 
-def append_json(user_id, inputs, file_name = './data/record.json'):
+def append_json(user_id, inputs, file_name = './data/record.json', to_return = False):
         with open(file_name, mode = 'r', encoding='utf-8') as file_in:
                 data = json.load(file_in)
         data.append({'user': user_id, 'text': inputs})
         with open(file_name, mode = 'w', encoding='utf-8') as file_out:
                 json.dump(data, file_out)
-        with open(file_name, mode = 'r', encoding='utf-8') as file_in:
-                return json.load(file_in)
+        if to_return:
+                with open(file_name, mode = 'r', encoding='utf-8') as file_in:
+                        return json.load(file_in)
