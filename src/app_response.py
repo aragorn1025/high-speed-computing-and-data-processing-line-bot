568f01f081a647894834c2db4f21019974607ff7
Aragorn Dai <tzjie.dai@gmail.com>
2018-06-06 23:57:16 +0800

    Test import json

--- src/app_response.py
+++ src/app_response.py
@@ -1,9 +1,20 @@
+import json
 import sys
 sys.path.append('./')
 import instruction
 
 def output(inputs):
+        head = 'json: ' + append_json(inputs = inputs) + '\n'
         try:
-                return instruction.get_instruction_response(inputs)
+                return head + instruction.get_instruction_response(inputs)
         except Exception as e:
                 return 'Error:\n' + str(e)
+
+def append_json(file_name = '../data/record.json', inputs):
+        with open(file_name, mode = 'r', encoding='utf-8') as file_in:
+                data = json.load(file_in)
+        data.append(inputs)
+        with open(file_name, mode = 'w', encoding='utf-8') as file_out:
+                json.dump(data, file_out)
+        with open(file_name, mode = 'r', encoding='utf-8') as file_in:
+                return json.load(file_in)
