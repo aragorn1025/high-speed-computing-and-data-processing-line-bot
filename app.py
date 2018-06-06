94d61e6c8d105858cff3cacb6479fe7bbce41595
Aragorn Dai <tzjie.dai@gmail.com>
2018-06-07 01:54:00 +0800

    Trying get requests

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 def handle_message(event):
         line_bot_api.reply_message(
 	        event.reply_token,
-                TextSendMessage(text = app_response.output(event.source, event.message.text))
+                TextSendMessage(text = app_response.output(event.source.user_id, event.message.text))
         )
 
 if __name__ == '__main__':
--- src/app_response.py
+++ src/app_response.py
@@ -3,17 +3,17 @@ import sys
 sys.path.append('./')
 import instruction
 
-def output(source, inputs):
+def output(user_id, inputs):
         try:
-                j = append_json(source, inputs)
+                j = append_json(user_id, inputs)
                 return str(j) + '\n' + instruction.get_instruction_response(inputs)
         except Exception as e:
                 return 'Error:\n' + str(e)
 
-def append_json(source, inputs, file_name = './data/record.json'):
+def append_json(user_id, inputs, file_name = './data/record.json'):
         with open(file_name, mode = 'r', encoding='utf-8') as file_in:
                 data = json.load(file_in)
-        data.append({'user': str(source.user_id), 'text': inputs})
+        data.append({'user': user_id, 'text': inputs})
         with open(file_name, mode = 'w', encoding='utf-8') as file_out:
                 json.dump(data, file_out)
         with open(file_name, mode = 'r', encoding='utf-8') as file_in:
