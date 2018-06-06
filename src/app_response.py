633412c4d244d04125f635cd7d037bf98807c9c3
Aragorn Dai <tzjie.dai@gmail.com>
2018-06-07 00:18:26 +0800

    Test import json

--- src/app_response.py
+++ src/app_response.py
@@ -10,7 +10,7 @@ def output(inputs):
                 head = 'json: ' + append_json(inputs = inputs) + '\n'
                 return listos + head + instruction.get_instruction_response(inputs)
         except Exception as e:
-                return 'Error:\n' + str(e)
+                return listos + 'Error:\n' + str(e)
 
 def append_json(file_name = '../data/record.json', inputs = ''):
         with open(file_name, mode = 'r', encoding='utf-8') as file_in:
