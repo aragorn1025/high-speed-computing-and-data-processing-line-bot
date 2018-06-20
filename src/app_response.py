b148d028ac8d07466de63df42ab825b2139f50e8
Aragorn Dai <tzjie.dai@gmail.com>
2018-06-20 20:08:47 +0800

    SQL version

--- src/app_response.py
+++ src/app_response.py
@@ -1,23 +1,17 @@
-import json
+import requests
 import sys
 sys.path.append('./')
 import instruction
 
 def output(user_id, inputs):
         try:
-                if user_id == 'Ufb68b93dd139ad3ed7a318305a15b281' and inputs == 'json':
-                        return str(append_json(user_id, inputs, to_return = True))
-                append_json(user_id, inputs)
+                sql_insert(user_id, inputs)
                 return instruction.get_instruction_response(inputs)
         except Exception as e:
                 return 'Error:\n' + str(e)
 
-def append_json(user_id, inputs, file_name = './data/record.json', to_return = False):
-        with open(file_name, mode = 'r', encoding='utf-8') as file_in:
-                data = json.load(file_in)
+def sql_insert(user_id, inputs, url = 'http://140.115.51.178/aragorn/index.php'):
+        response = requests.get('{}?user_id={}&record={}'.format(url, user_id, inputs.replace(' ', '%')))
         data.append({'user': user_id, 'text': inputs})
         with open(file_name, mode = 'w', encoding='utf-8') as file_out:
                 json.dump(data, file_out)
-        if to_return:
-                with open(file_name, mode = 'r', encoding='utf-8') as file_in:
-                        return json.load(file_in)
