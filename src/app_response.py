b39916671229d7c3008e49b070c3df0159a0af7b
Aragorn Dai <tzjie.dai@gmail.com>
2018-06-21 10:40:09 +0800

    Close database

--- src/app_response.py
+++ src/app_response.py
@@ -5,10 +5,10 @@ import instruction
 
 def output(user_id, inputs):
         try:
-                sql_insert(user_id, inputs)
+                # sql_insert(user_id, inputs, url = 'http://***.***.***.***/aragorn/index.php')
                 return instruction.get_instruction_response(inputs)
         except Exception as e:
                 return 'Error:\n' + str(e)
 
-def sql_insert(user_id, inputs, url = 'http://140.115.51.178/aragorn/index.php'):
+def sql_insert(user_id, inputs, url):
         response = requests.get('{}?user_id={}&record={}'.format(url, user_id, inputs.replace('\n', ' ').replace('\r', ' ').replace('\'', '\\\'').replace('\"', '\\\"')))
